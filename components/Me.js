7af341b6d40478a5d8f8b8cbb5058166852d27ba
abdulkadir-polmayan <abdulkadirsenel63@gmail.com>
2022-10-14 21:56:15 +0300

    redesigned

--- components/Me.js
+++ components/Me.js
@@ -22,25 +22,70 @@ const Me = () => {
 
   return (
     <div className="md:p-20 flex flex-col md:flex-row">
-      <div className="w-40 h-40 m-5 md:mt-[120px] bg-black">
+      {/* <div className="w-40 h-40 m-5 md:mt-[120px] bg-black">
         <img></img>
-      </div>
+      </div> */}
 
-      <header className="p-3 transition">
-        <div className="p-3 mb-5 ">
+      <header ref={parent} className="p-3 transition">
+        <div className="p-3 mb-5 flex justify-end">
           <Link href="/blogPosts">
-            <button className="bg-gradient-to-r from-indigo-200 via-blue-100/50 to-transparent decoration-indigo-500 p-4 rounded text-center text-lg font-black underline font-serif italic flex items-center select-none">
+            <button className="bg-gradient-to-r  from-indigo-200 via-blue-100/50 to-transparent decoration-indigo-500 p-4 rounded text-center text-lg font-black underline font-serif italic flex items-center select-none">
               Go to my blogs <BsChevronRight className="h-5 w-5 ml-4" />
             </button>
           </Link>
         </div>
         <h1 className="text-7xl font-serif">Abdulkadir Şenel</h1>
 
-        <div className="mt-10">
-          <h2 className="text-5xl font-serif italic font-bold ">I am junior</h2>
-          <div
-            ref={parent}
-            className={`p-2 rounded mt-5 ${dev && "bg-slate-100"}`}
+        <div className="mt-10 ">
+          <h2 className="text-5xl font-serif italic font-bold flex-initial ">
+            I am junior
+          </h2>
+          <h3 className="text-2xl font-serif underline mt-4">
+            Mobile App Developer
+          </h3>
+          <p className="leading-6 tracking-tight font-serif text-xl text-left py-2">
+            I am jr mobile app developer . <br></br> I know {"  "}
+            <a
+              className="underline decoration-blue-500 font-bold"
+              href="https://www.instapaper.com/u"
+            >
+              react
+            </a>{"-"}
+            {"  "}
+            <a
+              className="underline font-bold"
+              href="https://www.instapaper.com/u"
+            >
+              next.js
+            </a>{"-"}
+            {"  "}
+            <a
+              className="underline decoration-orange-500 font-bold"
+              href="https://www.instapaper.com/u"
+            >
+              sanity
+            </a>{" "}
+            {"-"}
+            <a
+              className="underline decoration-indigo-500 font-bold"
+              href="https://www.instapaper.com/u"
+            >
+              tailwind
+            </a>{" "}
+            {"-"}
+            <a
+              className="underline font-bold"
+              href="https://www.instapaper.com/u"
+            >
+              expo
+            </a>{" "}
+            
+            <br></br> I made some project
+            solve problems and my main perspective when developing app is make
+            it simple (understable) and solving problems.these are my projects.
+          </p>
+          {/* <div
+            className={`p-2 rounded mt-5 flex-initial ${dev && "bg-slate-100"}`}
           >
             <button
               onClick={() => {
@@ -56,17 +101,17 @@ const Me = () => {
                 <BsChevronDown className="h-5 w-5 ml-4" />
               </h3>
             </button>
-            {dev && (
-              <p className="leading-6 tracking-tight font-serif text-lg text-left py-2">
-                I am jr mobile app developer . I know <br></br> react - sanity -
-                next - tailwind - expo - firebase <br></br> I made some project
-                solve problems and my main perspective when developing app is
-                make it simple (understable) and solving problems.these are my
-                projects.
-              </p>
-            )}
           </div>
-          <div
+        </div>
+        {dev && (
+          <p className="leading-6 tracking-tight font-serif text-lg text-left py-2">
+            I am jr mobile app developer . I know <br></br> react - sanity -
+            next - tailwind - expo - firebase <br></br> I made some project
+            solve problems and my main perspective when developing app is make
+            it simple (understable) and solving problems.these are my projects.
+          </p>
+        )}
+        {/* <div
             ref={parent2}
             className={`p-2 rounded mt-5 ${elt && "bg-slate-100"}`}
           >
@@ -90,7 +135,7 @@ const Me = () => {
                 Democracy University.
               </p>
             )}
-          </div>
+           */}
         </div>
       </header>
     </div>

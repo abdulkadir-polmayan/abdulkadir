68e54fa73580479c4d2ef2da8e619300c3ea8e3c
abdulkadir-polmayan <abdulkadirsenel63@gmail.com>
2022-10-14 21:57:03 +0300

    d

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Me from "../components/Me";
 export default function Home() {
   return (
-    <div className="overflow-hidden min-h-screen">
+    <div className="overflow-hidden min-h-screen bg-white text-black">
       <Me />
     </div>
   );

f29106f482e27674a296a0786c7883012b603b40
abdulkadir-polmayan <abdulkadirsenel63@gmail.com>
2022-10-19 22:08:58 +0300

    rttrt

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Me from "../components/Me";
 export default function Home() {
   return (
-    <div className=" flex justify-center overflow-hidden min-h-screen bg-white text-black bg-gradient-to-t  from-indigo-100 via-blue-100/50 to-green-200/30 rounded m-2 md:m-12">
+    <div className=" flex justify-center overflow-hidden min-h-screen  text-black bg-gradient-to-b  from-indigo-100  to-green-100 rounded m-2 md:m-12">
       <Me />
     </div>
   );

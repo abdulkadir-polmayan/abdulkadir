a7d6864f4ac53b84b19d787f1f03f5f15f260503
abdulkadir-polmayan <83165182+abdulkadir-polmayan@users.noreply.github.com>
2022-10-22 22:12:52 +0300

    Update Me.js

--- components/Me.js
+++ components/Me.js
@@ -79,8 +79,8 @@ const Me = () => {
         >
           <div className="text-2xl flex flex-col items-start   ">
             <h2 className="font-serif italic font-bold">I am junior</h2>
-            <h3 className="text-3xl text-blue-500 font-sans font-bold">
-              Softwater sory software Developer
+            <h3 className="text-3xl text-black font-serif font-semibold">
+              Software Developer
             </h3>
           </div>
           <div className=" leading-6 tracking-tight font-serif text-xl text-left py-2">

66fa1d45e555ae8633e9313638b35c9d75e3554d
abdulkadir-polmayan <abdulkadirsenel63@gmail.com>
2022-11-03 07:57:16 +0300

    dcs

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
-    <div className=" flex justify-center overflow-hidden min-h-screen font-inter bg-green-200  text-green-900   md:pt-5">
+    <div className=" flex justify-center overflow-hidden min-h-screen pb-10 font-inter bg-green-200  text-green-900   md:pt-5">
       <Me />
       <Analytics />
 
